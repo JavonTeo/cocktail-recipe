60b748c9f365273fba21b3e7fc6aada905774c57
JavonTeo <javonteo@hotmail.com>
2023-05-12 00:03:29 +0800

    NOTFOUND message. Hover fn. Query alignment.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function getInfo() {
 function query(inputVal) {
     const URL = searchURL + inputVal;
     console.log(URL);
-    fetch(URL) //needs to be variable URL
+    fetch(URL)
     .then(response => response.json())
     .then(data => {
         console.log("FOUND");
@@ -27,17 +27,26 @@ function query(inputVal) {
         $("#search-result").empty();
         const drinksFound = data.drinks;
         console.log(drinksFound);
-        drinksFound.forEach(drink => {
-            const drinkLink = document.createElement('a');
-            drinkLink.className = "drink-figure";
-            const drinkImg = document.createElement('img');
-            const figCaption = document.createElement('figcaption');
-            drinkLink.appendChild(drinkImg);
-            drinkLink.appendChild(figCaption);
-            drinkImg.src = drink.strDrinkThumb;
-            figCaption.textContent = drink.strDrink;
-            searchResult.appendChild(drinkLink);
-        });
+        if (drinksFound == null) {
+            const cannotFindDrink = document.createElement('h2');
+            cannotFindDrink.textContent = "Sorry, we cannot find a drink matching that search.";
+            cannotFindDrink.style.color = "brown";
+            cannotFindDrink.style.margin = "auto";
+            cannotFindDrink.style.paddingTop = "5%"
+            searchResult.appendChild(cannotFindDrink);
+        } else {
+            drinksFound.forEach(drink => {
+                const drinkLink = document.createElement('a');
+                drinkLink.className = "drink-figure";
+                const drinkImg = document.createElement('img');
+                const figCaption = document.createElement('figcaption');
+                drinkLink.appendChild(drinkImg);
+                drinkLink.appendChild(figCaption);
+                drinkImg.src = drink.strDrinkThumb;
+                figCaption.textContent = drink.strDrink;
+                searchResult.appendChild(drinkLink);
+            });
+        }
     })
     .catch(error => console.error(error));
 }
